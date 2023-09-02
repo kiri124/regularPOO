47c5d2dc0ad85697f8896d77f9bf580226ce25d3
kiri124 <95322212+kiri124@users.noreply.github.com>
2023-09-01 21:26:40 -0500

    Add files via upload

---
+++ laboratorioPoo1/Circulo.java
@@ -0,0 +1,45 @@
+package laboratorioPoo1;
+
+public class Circulo extends FiguraGeometrica {
+
+private double radio;
+private Punto centro;
+
+void Circulo(String name, double radio, Punto centro){
+// Llamada al constructor de la superclase FiguraGeometrica con el nombre
+//super();
+    this.centro = centro;
+    this.radio = radio;
+}
+
+public void setCentro(Punto centro) {
+    this.centro = centro;
+}
+public void setRadio(double radio) {
+    this.radio = radio;
+}
+
+public Punto getCentro() {
+    return centro;
+}
+public double getRadio() {
+    return radio;
+}
+//todo es public en overrideeeeee
+@Override
+public double calcularaArea(){
+return Math.PI*radio*radio;
+}
+
+@Override
+public boolean esRegular(){
+    return true;
+}
+
+@override
+public String toString(){
+    return "Círculo: " + getNombre() + ", Centro: " + centro.toString() + ", Radio: " + radio;
+}
+
+}
+
